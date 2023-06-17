5d9855582c64b609d68ea27aea6d19fd002873d4
Dmitriy Sidorov <dima.sidorov.2003@list.ru>
2023-06-17 17:40:00 +0300

    added slash

--- string_consts.py
+++ string_consts.py
@@ -4,7 +4,8 @@ photo_source_text = 'Please, send me a photo that will be transformed according 
 error_generate = 'Two photos are required to generate a new one'
 
 start_command_description = "Start the bot and begin interaction"
-help_command_description = "This is a bot to which you can send two photos and receive a photo with a transferred style in response"
+help_command_description = "This is a bot to which you can send two photos and receive a photo with " \
+                           "a transferred style in response"
 set_style_command_description = "Choose a style for the photo"
 modified_photo_command_description = "Select a photo for style transfer"
 generate_command_description = "Generate a new photo"
